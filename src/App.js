f35e78df24f70e0117c5ac78f43ae2d9bc0dc249
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-19 12:39:15 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
 export default function App(){
-    return <h1>App component</h1>
+    return <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/products" element={<Products/>}/>
+    </Routes>
 }
