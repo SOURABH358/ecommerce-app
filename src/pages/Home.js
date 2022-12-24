ea92b72d77c9ee95a9880685001ed1d2714505bc
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-24 13:10:08 +0530

    added files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import Content from "../components/Content"
 import Footer from "../components/Footer"
+import Login from "../components/Login"
 export default function Home(){
     return <section id = "home__section" className="w-full h-full bg-light-grey">
         <Navbar/>
         <Hero/>
         <Content/>
         <Footer/>
+        {/* <Login/> */}
     </section>
 }
