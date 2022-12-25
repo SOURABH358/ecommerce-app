1a16fd832b65ee42a0b973dd89b0d04edfa238ba
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-25 21:37:23 +0530

    added files

---
+++ src/utils/data.js
@@ -0,0 +1,130 @@
+export const products=[
+    {
+        id:1,
+        brand: "Samsung",
+        name:"SAMSUNG Galaxy F13 (Sunrise Copper, 64 GB)  (4 GB RAM)",
+        categories:"Phones",
+        description:"Enjoy seamless connectivity and an uninterrupted movie marathon with the impressive Samsung Galaxy F13 that is designed specifically to impress the entertainment fanatics. This smartphone features a terrific 16.62 cm (6.6) FHD+ LCD Display that can effortlessly blow your mind with its incredible performance. Furthermore, this phone boasts a 50 MP Triple Camera setup that allows you to capture awesomeness with a gentle tap. Moreover, the Samsung Galaxy F13 sports up to 8 GB of RAM and features an innovative RAM plus technology that taps into the phone’s internal storage to elevate its performance.",
+        ratings:4.4,
+        originalPrice:14999,
+        price:11999,
+        discount:'20%',
+        thumbnail:'/assets/products/Phones/product-1/image-1.jpg',
+        images:[
+            '/assets/products/Phones/product-1/image-1.jpg',
+            '/assets/products/Phones/product-1/image-2.jpg',
+            '/assets/products/Phones/product-1/image-3.jpg',
+            '/assets/products/Phones/product-1/image-4.jpg'
+        ]
+    },
+    {
+        id:2,
+        brand: "VIVO",
+        name:"vivo T1 5G (Rainbow Fantasy, 128 GB)  (4 GB RAM)",
+        categories:"Phones",
+        description:"Be ready to encounter a thrilling user experience, improved mobile performance, and smooth gaming with the powerful Vivo T1 5G mobile phone. This mobile phone comes with a 16.72 cm (6.58) FHD in-cell display so that you can take delight in stunning visuals. Also, this mobile phone is equipped with a 50 MP Main Camera that enables you to capture stunning photos of everything around you. Lastly, courtesy of the 5000 mAh battery, you can keep running your phone, even during busy working schedules or while you are on a business trip.",
+        ratings:4.3,
+        originalPrice:19990,
+        price:15990,
+        discount:'20%',
+        thumbnail:'/assets/products/Phones/product-2/image-1.jpg',
+        images:[
+            '/assets/products/Phones/product-2/image-1.jpg',
+            '/assets/products/Phones/product-2/image-2.jpg',
+            '/assets/products/Phones/product-2/image-3.jpg',
+            '/assets/products/Phones/product-2/image-4.jpg'
+        ]
+    },
+    {
+        id:3,
+        brand: "realme",
+        name:"realme 10 Pro 5G (Dark Matter, 128 GB)  (6 GB RAM)",
+        categories:"Phones",
+        description:"Carry the Realme 10 Pro 5G with you wherever you go to catch people's attention. You can enjoy a large screen size with a significantly compact frame thanks to the 17.06 cm (6.72) screen and 93.76% screen-to-body ratio, and the 120 Hz refresh rate provides you with a fantastic user experience. Additionally, this smartphone enables exceptional photography and filmmaking abilities that let you record precious moments thanks to its 108 MP ProLight Camera. Furthermore, the Realme 10 Pro 5G's Snapdragon 695 5G Chipset is produced utilising a flawless 6 nm production process, which results in a power-efficient phone. Moreover, the Realme 10 Pro 5G's massive 8 GB + 8 GB Dynamic RAM design makes it simple to switch between programmes, play games, and access numerous files.",
+        ratings:4.4,
+        originalPrice:20999,
+        price:18999,
+        discount:'9%',
+        thumbnail:'/assets/products/Phones/product-3/image-1.jpg',
+        images:[
+            '/assets/products/Phones/product-3/image-1.jpg',
+            '/assets/products/Phones/product-3/image-2.jpg',
+            '/assets/products/Phones/product-3/image-3.jpg',
+            '/assets/products/Phones/product-3/image-4.jpg'
+        ]
+    },
+    {
+        id:4,
+        brand: "Xiaomi",
+        name:"Xiaomi 11i 5G (Stealth Black, 128 GB)  (8 GB RAM)",
+        categories:"Phones",
+        description:"Let yourself experience and immerse in the world of mesmerising smartphone experience with Xiaomi 11i 5G which comes loaded with mind-blowing features. This phone features a monstrous 5160 mAh battery with 67 W Turbo Charging capability that powers up the device up to 50% in about 13 minutes. Additionally, powered by the Mediatek Dimensity 920 5G processor, this phone offers a great deal of smoothness, performance, and efficiency in operation. Furthermore, the triple camera setup which comprises a 108 MP Camera, an 8 MP Ultra Wide Sensor, and a 2 MP Macro Camera allows you to capture stunning images which seize the memorable moments. Moreover, the 16.94 cm (6.67) AMOLED FHD+ (2400x1080) Punch Hole display with a 1.76 mm narrow bezel of this phone and versatile refresh rates of 120 Hz and 360 Hz enables you to stay on top of your gaming stint, allowing you to redefine smoothness.",
+        ratings:4.2,
+        originalPrice:31999,
+        price:26999,
+        discount:'15%',
+        thumbnail:'/assets/products/Phones/product-4/image-1.jpg',
+        images:[
+            '/assets/products/Phones/product-4/image-1.jpg',
+            '/assets/products/Phones/product-4/image-2.jpg',
+            '/assets/products/Phones/product-4/image-3.jpg',
+            '/assets/products/Phones/product-4/image-4.jpg'
+        ]
+    },
+    {
+        id:5,
+        brand: "Apple",
+        name:"APPLE iPhone 12 mini (Black, 64 GB)",
+        categories:"Phones",
+        description:"Dive into a world of crystal-clear visuals with the Super Retina XDR Display of the iPhone 12 Mini. This beast of a smartphone packs the A14 Bionic chip to make for blazing-fast performance speeds. On top of that, its Dual-camera System, along with Night Mode, helps you click amazing pictures and selfies even when the lighting isn’t as good as you’d want it to be.",
+        ratings:4.5,
+        originalPrice:59900,
+        price:37999,
+        discount:'36%',
+        thumbnail:'/assets/products/Phones/product-5/image-1.jpg',
+        images:[
+            '/assets/products/Phones/product-5/image-1.jpg',
+            '/assets/products/Phones/product-5/image-2.jpg',
+            '/assets/products/Phones/product-5/image-3.jpg',
+            '/assets/products/Phones/product-5/image-4.jpg'
+        ]
+    },
+    {
+        id:6,
+        brand: "OPPO",
+        name:"OPPO Reno8 5G (Shimmer Black, 128 GB)  (8 GB RAM)",
+        categories:"Phones",
+        description:"Discover new possibilities and enjoy the mind-blowing performance with the innovative Oppo Reno8 smartphone, which is loaded with impressive features. This phone features Dual Sony Sensors on the front and back of the phone, allowing you to create captivating images with beautiful visuals. Furthermore, the slim form of this phone, which has a thickness of 7.67 mm and weighs roughly 179 g, allows you to create a fashion statement that draws the attention of onlookers. Furthermore, you can enjoy smooth operation and an exquisite user experience owing to the fantastic MediaTek Dimensity 1300 with a magnificent 6 nm architecture.",
+        ratings:4.3,
+        originalPrice:38999,
+        price:29999,
+        discount:'23%',
+        thumbnail:'/assets/products/Phones/product-6/image-1.jpg',
+        images:[
+            '/assets/products/Phones/product-6/image-1.jpg',
+            '/assets/products/Phones/product-6/image-2.jpg',
+            '/assets/products/Phones/product-6/image-3.jpg',
+            '/assets/products/Phones/product-6/image-4.jpg'
+        ]
+    },
+]
+/*
+{
+    id:6,
+    brand: "",
+    name:"",
+    categories:"",
+    description:"",
+    ratings:,
+    originalPrice:,
+    price:,
+    discount:'',
+    thumbnail:'/assets/products/Phones/product-2/image-1.jpg',
+    images:[
+        '/assets/products/Phones/product-2/image-1.jpg',
+        '/assets/products/Phones/product-2/image-2.jpg',
+        '/assets/products/Phones/product-2/image-3.jpg',
+        '/assets/products/Phones/product-2/image-4.jpg'
+    ]
+},
+*/
