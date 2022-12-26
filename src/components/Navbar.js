3f68abfdeb0743601e497d6485de15516419d3ad
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-26 21:19:49 +0530

    added files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,15 @@ import { doc, setDoc } from "firebase/firestore";
 import { db } from "../firebase/Firebase";
 import { GetAuthContext } from "../Reducer/AuthContext";
 import { Link } from "react-router-dom";
-
+import { products } from "../utils/data"
+import { GetProdContext } from "../Reducer/productContext";
 export default function Navbar() {
     const [menu, showMenu] = useState(false)
+    const [query, setQuery] = useState(null)
     const currentUser = GetAuthContext();
-    console.log(currentUser)
+    const {setCurrentProd} = GetProdContext();
     // const [menu, showMenu] = useState(false)
+    console.log(query)
     async function handleLogin() {
         await signInWithPopup(auth, provider)
             .then(async (result) => {
@@ -36,14 +39,33 @@ export default function Navbar() {
             console.log(error)
         });
     }
+    function handleChange(e) {
+        if (e.code == 'Backspace') {
+            setQuery(null)
+        }
+        else if (e.code == 'Enter') {
+            let regex = new RegExp(e.target.value, 'i');
+            setQuery(products.filter(item => item.name.match(regex)))
+            console.log(e.target.value)
+        }
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+    }
+
     return <nav className="w-full h-[6rem] bg-gradient-to-r from-purple to-pink px-8 flex items-center justify-between">
         <p className="text-[white] text-[1.5rem]">Shopzy</p>
-        <form className="rounded-md bg-[white] overflow-hidden">
-            <input type="search" name="search" className="w-[10rem] md:w-[50vw] md:h-10 focus:outline-none px-4 leading-8"
-                placeholder="search..." />
+        <form className="rounded-md bg-[white] relative" onSubmit={(e)=>handleSubmit(e)}>
+            <input type="search" name="search" className="w-[10rem] md:w-[50vw] rounded-md capitalize md:h-10 focus:outline-none px-4 leading-8"
+                placeholder="search..." onKeyDown={(e) => handleChange(e)} />
             <button className="w-[2.5rem]" type="button">
                 <BsSearch className="text-purple" />
             </button>
+            {query ? <div className=" absolute top-[3rem] left-0 w-full py-[1rem] bg-[white] shadow-lg z-20">
+                {query.map(item => {
+                    return <div className="py-[1rem] px-[1rem] text-[black] cursor-pointer" onClick={()=>setCurrentProd(item.id)}><Link to={`products/${item.id}`}>{item.name}</Link></div>
+                })}
+            </div> : null}
         </form>
         <div className="hidden md:flex text-[white] gap-6 items-center">
             {currentUser ? null : <p className="md:block cursor-pointer px-2 py-1 border-2 border-[white] 
