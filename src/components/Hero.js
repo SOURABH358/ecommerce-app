5243e7fb3eafa892ec25cda8e6267cd24d8c81ad
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-27 12:30:58 +0530

    added files

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,15 +2,14 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from "swiper";
 import 'swiper/css';
 import "swiper/css/pagination";
+import { categories } from '../utils/data';
 export default function Hero() {
-    return <section id="hero__section" className="w-full">
-        <p className="px-8 py-2 md:block hidden">Categories |
-            <span className="px-4">Women's Wear</span> |
-            <span className="px-4">Men's Wear</span> |
-            <span className="px-4">Electronics</span> |
-            <span className="px-4">Groceries</span> |
-            <span className="px-4">Sports Wear</span> |
-            <span className="px-4">Furnitures</span>
+    return <section id="hero__section" className="w-full bg-[white]">
+        <p className="px-8 py-2 md:block hidden">
+            <span className='px-4'>Categories</span>
+            {categories.map(item=>{
+                return <span className='px-4 border-r-2'>{item}</span>
+            })}
         </p>
         <select className='py-2 px-2 my-2 mx-[2.5%] w-[95%] focus:outline-none md:hidden'>
             <option>Categories</option>
@@ -21,7 +20,7 @@ export default function Hero() {
             <option>Sports Wear</option>
             <option>Furnitures</option>
         </select>
-        <Swiper className="mySwiper md:h-[20rem] h-[15rem]"
+        <Swiper className="mySwiper md:h-[20rem] h-[20rem]"
             slidesPerView={1}
             allowTouchMove={false}
             centeredSlides={true}
@@ -42,7 +41,7 @@ export default function Hero() {
         >
             <SwiperSlide className="">
                 <div className="bg-gradient-to-br from-purple to-pink h-full">
-                    <img className='w-full ' src='/assets/banners/banner-1.jpg' alt="banner-1"/>
+                    <img className='w-full h-full' src='/assets/banners/banner-1.jpg' alt="banner-1"/>
                 </div></SwiperSlide>
             <SwiperSlide className="">
                 <div className="bg-pink h-full">
@@ -51,7 +50,7 @@ export default function Hero() {
             </SwiperSlide>
             <SwiperSlide className="">
                 <div className="bg-pink h-full">
-                    <img className='w-full h-full' src='/assets/banners/banner-3.jpg' alt="banner-3"/>
+                    <img className='w-full h-full object-fill' src='/assets/banners/banner-3.jpg' alt="banner-3"/>
                 </div>
             </SwiperSlide>
             <SwiperSlide className="">
