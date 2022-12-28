ece8e25f5550358710f7666b3d6c9046bdef9dbe
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-28 13:35:49 +0530

    added files

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -4,9 +4,9 @@ import { categories } from "../utils/data";
 import { BsFillStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 import { GetProdContext } from "../Reducer/productContext";
 export default function Categories() {
-    const {currentCat, setCurrentCat} = GetProdContext();
+    const { currentCat, setCurrentCat, setCurrentProd } = GetProdContext();
     console.log(currentCat)
-    
+
     return <section className="w-full min-h-[100vh] bg-light-grey">
         <Navbar />
         <p className="py-2 px-4 bg-[white] mx-2 rounded-md mt-2">
@@ -20,37 +20,40 @@ export default function Categories() {
                 <div >
                     {categories.map((item, i) => {
                         return <p key={i * 201} className={`py-2 px-4 hover:bg-light-purple cursor-pointer my-4 
-                        rounded-md ${item === currentCat ? 'bg-purple text-[white]' : null}`} onClick={()=>setCurrentCat(item)}>{item}</p>
+                        rounded-md ${item === currentCat ? 'bg-purple text-[white]' : null}`} onClick={() => setCurrentCat(item)}>{item}</p>
                     })}
                 </div>
             </div>
             <div className="w-full px-4 flex flex-wrap gap-8 md:justify-center">
                 {products.map(item => {
                     return item.categories === currentCat ?
-                        <div key={item.id*203} className="md:w-[22rem] w-full p-4 h-[30rem] rounded-xl shadow-2xl bg-[white] cursor-pointer">
-                            <div className="w-full flex justify-center border-light-purple border-2 rounded-md py-4">
+                        <div key={item.id * 203} className="md:w-[22rem] w-full p-4 h-[30rem] rounded-xl shadow-2xl bg-[white] cursor-pointer"
+                            onClick={() => setCurrentProd(item.id)}>
+                            <Link to={`products/${item.id}`}><div className="w-full flex justify-center border-light-purple border-2 rounded-md py-4">
                                 <img src={item.thumbnail} alt="category-product" className="h-[15rem]" />
                             </div>
-                            <p className="text-center mt-4 h-[3rem]">{item.name}</p>
-                            {/* <p className="text-[grey] text-[0.9rem] mt-2">{item.description.length > 50 ? item.description.slice(0, 50) : item.description}</p> */}
-                            <div className="flex justify-between items-start pt-4">
-                                <div className="flex gap items-center">
-                                    <p className="text-dark-green pr-2">{item.ratings}</p>
-                                    {item.ratings > 0 ? item.ratings >= 1 ? <BsFillStarFill className="text-dark-green" /> : <BsStarHalf className="text-dark-green" /> : <BsStar className="text-dark-green" />}
-                                    {item.ratings > 1 ? item.ratings >= 2 ? <BsFillStarFill className="text-dark-green" /> : <BsStarHalf className="text-dark-green" /> : <BsStar className="text-dark-green" />}
-                                    {item.ratings > 2 ? item.ratings >= 3 ? <BsFillStarFill className="text-dark-green" /> : <BsStarHalf className="text-dark-green" /> : <BsStar className="text-dark-green" />}
-                                    {item.ratings > 3 ? item.ratings >= 4 ? <BsFillStarFill className="text-dark-green" /> : <BsStarHalf className="text-dark-green" /> : <BsStar className="text-dark-green" />}
-                                    {item.ratings > 4 ? item.ratings >= 5 ? <BsFillStarFill className="text-dark-green" /> : <BsStarHalf className="text-dark-green" /> : <BsStar className="text-dark-green" />}
-                                </div>
-                                <div className="flex gap-x-2">
-                                    <p className="px-2 h-fit rounded-md bg-light-purple text-purple font-bold">{item.discount} off</p>
-                                    <div >
-                                        <p className="font-bold">{item.price}</p>
-                                        <p className="line-through text-[grey]">{item.originalPrice}</p>
+                                <p className="text-center mt-4 h-[3rem]">{item.name}</p>
+                                {/* <p className="text-[grey] text-[0.9rem] mt-2">{item.description.length > 50 ? item.description.slice(0, 50) : item.description}</p> */}
+                                <div className="flex justify-between items-start pt-4">
+                                    <div className="flex gap items-center">
+                                        <p className="text-dark-green pr-2">{item.ratings}</p>
+                                        {item.ratings > 0 ? item.ratings >= 1 ? <BsFillStarFill className="text-dark-green" /> : <BsStarHalf className="text-dark-green" /> : <BsStar className="text-dark-green" />}
+                                        {item.ratings > 1 ? item.ratings >= 2 ? <BsFillStarFill className="text-dark-green" /> : <BsStarHalf className="text-dark-green" /> : <BsStar className="text-dark-green" />}
+                                        {item.ratings > 2 ? item.ratings >= 3 ? <BsFillStarFill className="text-dark-green" /> : <BsStarHalf className="text-dark-green" /> : <BsStar className="text-dark-green" />}
+                                        {item.ratings > 3 ? item.ratings >= 4 ? <BsFillStarFill className="text-dark-green" /> : <BsStarHalf className="text-dark-green" /> : <BsStar className="text-dark-green" />}
+                                        {item.ratings > 4 ? item.ratings >= 5 ? <BsFillStarFill className="text-dark-green" /> : <BsStarHalf className="text-dark-green" /> : <BsStar className="text-dark-green" />}
+                                    </div>
+                                    <div className="flex gap-x-2">
+                                        <p className="px-2 h-fit rounded-md bg-light-purple text-purple font-bold">{item.discount} off</p>
+                                        <div >
+                                            <p className="font-bold">{item.price}</p>
+                                            <p className="line-through text-[grey]">{item.originalPrice}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                                <p className="text-left text-[0.9rem] mt-4 text-purple border-purple border-[1px] w-fit px-2 rounded-md">Shopzy certified</p>
+                                <p className="text-left text-[0.9rem] mt-4 text-purple border-purple border-[1px] w-fit px-2 
+                                rounded-md">Shopzy certified</p>
+                            </Link>
                         </div> : null
                 })}
             </div>
